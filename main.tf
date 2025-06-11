345ba6fab2bb415548097d92476a7fccf24be4a9
Snekhapugal <SnekhaPuhazendhi>
2025-06-11 12:39:11 +0530

    network user blockchange

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ resource "google_dataproc_cluster" "mycluster" {
      ]
   }
   depends_on = [ google_project_iam_binding.kms_dataproc,
-                 google_project_iam_binding.network_binding1000,
                  google_project_iam_binding.serviceaccount_access,
                   ]
 
@@ -82,16 +81,7 @@ data "google_project" "service_project" {
   project_id = var.project
 }
 
-resource "google_project_iam_binding" "network_binding1000" {
-  count   = 1
-  project = var.host_project
-  role    = "roles/compute.networkUser"
-  members = [
-    "serviceAccount:service-${data.google_project.service_project.number}@dataproc-accounts.iam.gserviceaccount.com",
-    "serviceAccount:service-${data.google_project.service_project.number}@compute-system.iam.gserviceaccount.com",
-    
-  ]
-}
+
 resource "google_project_iam_binding" "kms_dataproc" {
   count   = 1
   project =var.project
